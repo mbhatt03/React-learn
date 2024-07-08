bd46266660dfbd6647271f77e37cda83a0342ffd
mbhatt03 <mihirbhatt03@gmail.com>
2024-07-08 12:02:25 +0530

    first

--- 01react-hooks/src/App.jsx
+++ 01react-hooks/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
+import React , { useState } from 'react'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const addMore = () => {
+    if(count<=19){
+      setCount(count+1);
+    }
+  }
+  const addLess = () => {
+    if (count >0){
+      setCount(count-1);
+    }
+  }
+
   return (
     <>
-      <h1 className=' bg-violet-600 text-white text-3xl p-3 '>JI SWAGAT NHI KAROGE HUMARA</h1>
+      <h1 className=' bg-violet-600 text-white text-3xl p-3 '>JI SWAGAT NHI KAROGE HUMARA {count} </h1>
+      <button className=' border-spacing-5 rounded-xl p-4 border-yellow-950' onClick={addMore}>Once More</button>
+      <br />
+      <button className=' border-spacing-5 rounded-xl p-4 border-yellow-950' onClick={addLess}>One Less</button>
     </>
   )
 }
